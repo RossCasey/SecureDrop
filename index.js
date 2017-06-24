0e0f324773c3b41a7fddffd0d3a667b83be5e63c
Ross Casey <rosscasey@users.noreply.github.com>
2017-06-24 20:50:06 +0100

    Add tests for drop controller

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-const DB = require('./Persistence/database');
+const DB = require('./src/Persistence/database');
 require('dotenv').config();
-const App = require('./app');
+const App = require('./src/app');
 
 DB.init().then((db) => {
   App(db).listen(3000);
---
+++ test/unit/Controllers/test-drop-controller.js
@@ -0,0 +1,123 @@
+const assert = require('assert');
+const DropController = require('../../../src/Controllers/DropController');
+const sinon = require('sinon');
+
+describe('DropController', () => {
+
+  describe('create', () => {
+
+    it('should call create on the database with a generated id and the given ciphertext', () => {
+      const db = {
+        drop: {
+          create: sinon.stub().returns(Promise.resolve())
+        }
+      };
+
+      const dropController = DropController(db);
+
+      return dropController.create("abc").then(() => {
+        assert.ok(db.drop.create.called);
+        assert.ok(db.drop.create.calledWith(sinon.match.any, "abc"));
+      });
+    });
+
+    it('should call return a rejected promise if the database return rejected promise', () => {
+      const db = {
+        drop: {
+          create() {
+            return Promise.reject(new Error('error'));
+          }
+        }
+      };
+
+      const dropController = DropController(db);
+
+      return dropController.create("abc").then(() => {
+        return Promise.reject(new Error('should not get here'));
+      }).catch((err) => {
+        assert.strictEqual(err.message, 'error');
+        return Promise.resolve();
+      });
+    });
+  });
+
+  describe('get', () => {
+
+    it('should call get and claim on the database when get called with id', () => {
+      const db = {
+        drop: {
+          get: sinon.stub().returns(Promise.resolve({cipherText: 'def'})),
+          claim: sinon.stub().returns(Promise.resolve())
+        }
+      };
+
+      const dropController = DropController(db);
+
+      return dropController.get('abc').then((cipherText) => {
+        assert.strictEqual(cipherText, 'def');
+        assert.ok(db.drop.get.called);
+        assert.ok(db.drop.get.calledWith('abc'));
+        assert.ok(db.drop.claim.called);
+        assert.ok(db.drop.claim.calledWith('abc'));
+      });
+    });
+
+    it('should return empty promise if database returns nothing', () => {
+      const db = {
+        drop: {
+          get: sinon.stub().returns(Promise.resolve()),
+          claim() { assert.ok(false, 'should not be called')}
+        }
+      };
+
+      const dropController = DropController(db);
+
+      return dropController.get('abc').then((cipherText) => {
+        assert.ok(cipherText === undefined);
+        assert.ok(db.drop.get.called);
+        assert.ok(db.drop.get.calledWith('abc'));
+      })
+    });
+
+    it('should return rejected promise if get throws error', () => {
+      const db = {
+        drop: {
+          get() {
+            return Promise.reject(new Error('error'));
+          }
+        }
+      };
+
+      const dropController = DropController(db);
+
+      return dropController.get('abc').then((cipherText) => {
+        return Promise.reject(new Error('should not get here'));
+      }).catch((err) => {
+        assert.strictEqual(err.message, 'error');
+        return Promise.resolve();
+      });
+    });
+
+    it('should return rejected promise if claim returns rejected errors', () => {
+      const db = {
+        drop: {
+          get: sinon.stub().returns(Promise.resolve({cipherText: 'def'})),
+          claim() {
+            return Promise.reject(new Error('error'));
+          }
+        }
+      };
+
+      const dropController = DropController(db);
+
+      return dropController.get('abc').then((cipherText) => {
+        return Promise.reject(new Error('should not get here'));
+      }).catch((err) => {
+        assert.ok(db.drop.get.called);
+        assert.ok(db.drop.get.calledWith('abc'));
+        assert.strictEqual(err.message, 'error');
+        return Promise.resolve();
+      });
+    });
+  });
+});
