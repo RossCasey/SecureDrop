7903cd8bcfc818081a59ea6edb4a1836bb18e44f
Ross Casey <rosscasey@users.noreply.github.com>
2017-06-30 21:17:19 +0100

    Simple react setup working

--- src/server/app.js
+++ src/server/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const DropRouter = require('./Routers/DropRouter');
+const path = require('path');
 
 module.exports = (db) => {
 
@@ -14,6 +15,12 @@ module.exports = (db) => {
     app.use('/test', express.static('test/client/index.html'));
     app.use('/test/script.js', express.static('test/client/test-bundle.js'));
   }
+
+  app.use('/', express.static('src/client/public'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../client/public/index.html'));
+  });
 
   return app;
 };
